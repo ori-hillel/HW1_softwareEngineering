b48ae12075aaa5b9ed5e15619ac77b7bf1b1f1bd
Ori Hillel <orihillel@campus.technion.ac.il>
2023-05-14 19:56:50 +0300

    "Action" class implemented

--- src/Action.java
+++ src/Action.java
@@ -1,2 +1,24 @@
 public class Action {
+    private Tile tile;
+    private Enum direction;
+
+    public Action(Tile tile, Enum direction) {
+        this.tile = tile;
+        this.direction = direction;
+    }
+    public Enum getDirection() {
+        return direction;
+    }
+    public void setDirection(Enum otherDirection) {
+        this.direction = otherDirection;
+    }
+    public Tile getTile() {
+        return tile;
+    }
+    public void setTile(Tile otherTile) {
+        this.tile = otherTile;
+    }
+    public String toString() {
+        return String.format("Move %d %s", tile.getValue(), direction.getDirection().toLowerCase());
+    }
 }
