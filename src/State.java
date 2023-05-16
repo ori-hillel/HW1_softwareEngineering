94bb69d6ebb53f4f22fa58cfa7e4a556176e0465
idanArkav <133385135+idanArkav@users.noreply.github.com>
2023-05-16 17:03:18 +0300

    Result added

--- src/State.java
+++ src/State.java
@@ -1,8 +1,9 @@
 public class State {
-    private static final Enum UP="UP"
-    private static final Enum DOWN="DOWN"
-    private static final Enum LEFT="LEFT"
-    private static final Enum RIGHT="RIGHT"
+    private static final Enum UP = new Enum("UP");
+    private static final Enum DOWN=new Enum("DOWN")
+    private static final Enum LEFT= new Enum("LEFT");
+    private static final Enum RIGHT=new Enum("RIGHT");
+    private static final int EMPTY=0;
 private Board board;
     @Override
     public boolean equals(Object other) {
@@ -22,23 +23,29 @@ private Board board;
     }
     public State result(Action action) {
         State res = new State()
-    for(int i = 0;i<this.board.getTiles.length;i++)
-        for(int j = 0;j<this.board.getTiles.length;j++)
+    int rowIndex=0,colIndex=0;
+    for(int i = 0;i<this.board.getTiles().length;i++)
+        for(int j = 0;j<this.board.getTiles().length;j++)
         {
-            if(this.board.getTiles == 0)
+            if(this.board.getTiles()[i][j].getValue() == EMPTY)
+            {
+                colIndex = j;
+                rowIndex = i;
+                break;
+            }
 
         }
     if(action.getDirection().equals(UP)) {
-
+        Board.swap(this.board.getTiles()[rowIndex][colIndex],this.board.getTiles()[rowIndex+1][colIndex]);
     }
     if(action.getDirection().equals(DOWN)) {
-
+        Board.swap(this.board.getTiles()[rowIndex+1][colIndex],this.board.getTiles()[rowIndex-1][colIndex]);
     }
     if(action.getDirection().equals(LEFT)) {
-
+        Board.swap(this.board.getTiles()[rowIndex][colIndex],this.board.getTiles()[rowIndex][colIndex+1]);
     }
     if(action.getDirection().equals(RIGHT)) {
-
+        Board.swap(this.board.getTiles()[rowIndex][colIndex],this.board.getTiles()[rowIndex][colIndex-1]);
     }
 
 
