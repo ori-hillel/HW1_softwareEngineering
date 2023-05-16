8d2fd7e4b530bf62fac77da7f4aa213bfc7b4726
idanArkav <133385135+idanArkav@users.noreply.github.com>
2023-05-16 16:43:20 +0300

    Action class updated

--- src/State.java
+++ src/State.java
@@ -1,4 +1,8 @@
 public class State {
+    private static final Enum UP="UP"
+    private static final Enum DOWN="DOWN"
+    private static final Enum LEFT="LEFT"
+    private static final Enum RIGHT="RIGHT"
 private Board board;
     @Override
     public boolean equals(Object other) {
@@ -16,5 +20,30 @@ private Board board;
     public boolean isGoal() {
         return this.board.equals(this.board.goalBoard());
     }
+    public State result(Action action) {
+        State res = new State()
+    for(int i = 0;i<this.board.getTiles.length;i++)
+        for(int j = 0;j<this.board.getTiles.length;j++)
+        {
+            if(this.board.getTiles == 0)
+
+        }
+    if(action.getDirection().equals(UP)) {
+
+    }
+    if(action.getDirection().equals(DOWN)) {
+
+    }
+    if(action.getDirection().equals(LEFT)) {
+
+    }
+    if(action.getDirection().equals(RIGHT)) {
+
+    }
+
+
+
+    }
+
 
 }
