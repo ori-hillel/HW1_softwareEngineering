e530a8bdf48ae2e8af8580e3a6fd4b62f5e8ace8
Ori Hillel <orihillel@campus.technion.ac.il>
2023-05-23 18:42:04 +0300

    Added documentation to the Action class

--- src/Action.java
+++ src/Action.java
@@ -1,4 +1,8 @@
 public class Action {
+    /**
+     * The action class represents a made move.
+     * An action consists of a direction, and the tile which the move was made on.
+     */
     private Tile tile;
     private Enum direction;
 
@@ -24,6 +28,10 @@ public class Action {
         this.tile = otherTile;
     }
 
+    /**
+     * An override to the  Object Class's toString() method.
+     * @return a string, which describes the action.
+     */
     public String toString() {
         if (this.direction != null)
             return String.format("Move %d %s", tile.getValue(), direction.getDirection().toLowerCase());
