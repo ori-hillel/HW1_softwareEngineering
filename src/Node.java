295f5450f37f4f0b75446bbaf3f216c8237dd837
Ori Hillel <orihillel@campus.technion.ac.il>
2023-05-23 18:39:10 +0300

    Added documentation to the Node class

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,8 @@
 public class Node {
+    /**
+     * represents a node, which is substantial for the Greedy First BEst search.
+     * Each node consists of the node's state, the previous action, and the root node.
+     */
     private State state;
     private Node root;
     private Action action;
@@ -28,6 +32,10 @@ public class Node {
         this.action = action;
     }
 
+    /**
+     * expands a node ti its children, according to the possible moves that are available from the current node's state.
+     * @return a 1D array of nodes, which represents the node's children.
+     */
     public Node[] expand() {
         Action actions[] = this.state.actions();
         Node[] ret = new Node[actions.length];
@@ -40,111 +48,28 @@ public class Node {
         return ret;
     }
 
+    /**
+     * calculates the node's heuristic value.
+     * The chosen heuristic function is the misplaced tiles heuristic function.
+     * @return an integer, which is the result of the heuristic function for this node.
+     */
     public int heuristicValue() {
         return misplacesTiles();
     }
-    private int linearConflicts() {
-        int goalValue = 0, goalRow = 0, goalColumn = 0, currentValue = 0;
-        Tile[][] trans = transpose();
-        Tile[][] tiles = this.state.getBoard().getTiles();
-        int numRows = this.state.getBoard().getTiles().length;
-        int numCols = this.state.getBoard().getTiles()[0].length;
-        int linearConflicts = 0;
-        for (int row = 0; row < numRows; row++) {
-            linearConflicts += totalConflictsInLine(tiles[row]); // conflicts in a certain row
-            for (int column = 0; column < numCols; column++) {
-                if (row == 0)
-                    linearConflicts += totalConflictsInLine(trans[column]); // conflicts in a certain column
-                currentValue = tiles[row][column].getValue();
-                goalRow = calcRowInGoalBoard(currentValue);
-                goalColumn = calcColumnInGoalBoard(currentValue);
-                linearConflicts += calcManhattanDistance(row, column, goalRow, goalColumn); // adds Manhattan distance of a tile from its goal position.
-            }
-        }
-        return linearConflicts;
-    }
-    private int totalConflicts() {
-        int totalConflicts = 0;
-        Tile[][] tiles = this.state.getBoard().getTiles();
-        for (int row = 0; row < tiles.length; row++)
-            totalConflicts += totalConflictsInLine(tiles[row]);
-        for (int column = 0; column < transpose().length; column++)
-            totalConflicts += totalConflictsInLine(transpose()[column]);
-        return totalConflicts;
-        }
-    private Tile[][] transpose() {
-        Tile[][] thisTiles = this.state.getBoard().getTiles();
-        int numRows = this.state.getBoard().getTiles().length;
-        int numCols = this.state.getBoard().getTiles()[0].length;
-        Tile[][] trans = new Tile[numCols][numRows];
-        for (int row = 0; row < numRows; row++)
-            for (int column = 0; column < numCols; column++)
-                trans[column][row] = new Tile(thisTiles[row][column].getValue());
-        return trans;
-    }
-        private int totalConflictsInLine(Tile[] line) {
-        int conflicts = 0;
-        int val1, val2;
-        for (int first = 0; first < line.length - 1; first++)
-            for (int second = first; second < line.length; second++) {
-                val1 = line[first].getValue();
-                val2 = line[second].getValue();
-                if ((line[first].getValue() > line[second].getValue())
-                        && (calcRowInGoalBoard(val1) == calcRowInGoalBoard(val2) || calcColumnInGoalBoard(val1) == calcColumnInGoalBoard(val2)))
-                    conflicts++;
-            }
-        return conflicts;
-    }
-    private int calcRowInGoalBoard (int value) {
-        if (value != 0)
-            return ((value - 1) / this.getState().getBoard().getTiles()[0].length);
-        return this.state.getBoard().getTiles().length - 1;
-    }
-    private int calcColumnInGoalBoard (int value) {
-        if (value != 0)
-            return value - (calcRowInGoalBoard(value)*this.state.getBoard().getTiles()[0].length);
-        return this.state.getBoard().getTiles()[0].length;
-    }
-    private int sumOfManhattanDistances () {
-        int sum = 0;
-        Board board = this.state.getBoard();
-        Board goalBoard = board.goalBoard();
-        int currentValue=0, indexRow=0, indexCol=0, indexGoalRow=0, indexGoalCol=0;
-        Tile currentTile;
-        for (int row = 0; row < board.getTiles().length; row++) {
-            for (int col = 0; col < board.getTiles()[0].length; col++) {
-                currentValue = col + row * board.getTiles().length;
-                currentTile = new Tile(currentValue);
-                indexRow = board.getRow(currentTile);
-                indexCol = board.getColumn(currentTile);
-                indexGoalRow = goalBoard.getRow(currentTile);
-                indexGoalCol = goalBoard.getColumn(currentTile);
-                sum += calcManhattanDistance(indexRow, indexCol, indexGoalRow, indexGoalCol);
-            }
-        }
-        return sum;
-    }
+
+    /**
+     * calculates how many misplaced tiles are within the node's board.
+     * @return an integer, which is the number of misplaced tiles.
+     */
     private int misplacesTiles() {
         Board goalBoard = this.state.getBoard().goalBoard();
         Board thisBoard = this.state.getBoard();
         int counter = 0;
         for (int row = 0; row < thisBoard.getTiles().length; row++)
             for (int column = 0; column < thisBoard.getTiles()[0].length; column++) {
-                if (!thisBoard.getTiles()[row][column].equals(goalBoard.getTiles()[row][column]))
+                if (!thisBoard.getTiles()[row][column].equals(goalBoard.getTiles()[row][column])) // if the tile thisBoard[row][column] is misplaced
                     counter++;
             }
         return counter;
-    }
-    private static int calcManhattanDistance(int x1, int y1, int x2, int y2) {
-        int distance = absoluteDifference(x1, x2) + absoluteDifference(y1, y2);
-        return distance;
-    }
-
-    private static int absoluteDifference(int a, int b) {
-        if (a > b) {
-            return a - b;
-        } else {
-            return b - a;
-        }
     }
 }
